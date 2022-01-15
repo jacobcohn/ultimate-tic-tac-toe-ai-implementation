0ed3a5fa9c9f1cc47ebffa2607a3dc6480a51172
Jacob Cohn <jacobnc916@gmail.com>
2022-01-15 13:52:57 -0600

    finished Header and Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 import "../styles/Footer.css";
 
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>footer</h1>
+      <h1 className="footerText">Created by Jacob Cohn</h1>
+      <a href="https://github.com/jacobcohn" target="_blank" rel="noreferrer">
+        <FaGithub className="githubIcon" />
+      </a>
     </div>
   );
 }
